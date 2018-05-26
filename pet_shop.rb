71bdaf2dfaa36daf042f2b911501118c6e90f4ec
jamos90 <jamos_98@hotmail.com>
2018-05-26 12:20:19 +0100

    test 5 passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -10,13 +10,17 @@ def add_or_remove_cash(name,ammount)
   name[:admin][:total_cash] += ammount
 end
 
-def pets_sold(@pet_shop)
-  
+def pets_sold(number_of_pets)
+  return @pet_shop[:admin][:pets_sold]
 end
 
+def increase_pets_sold(number_of_pets,ammount)
+  @pet_shop[:admin][:pets_sold] += ammount
+end
 
-
-
+def stock_count(@pet_shop)
+  
+end
 
 
 # @pet_shop = {
