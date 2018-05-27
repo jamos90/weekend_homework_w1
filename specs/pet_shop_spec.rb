b9857be42e3556a57555da1c7cb93dfc80a6665f
jamos90 <jamos_98@hotmail.com>
2018-05-27 11:56:06 +0100

    all non optional test passed

--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -195,13 +195,17 @@ class TestPetShop < Minitest::Test
   # def test_sell_pet_to_customer__pet_found
   #   customer = @customers[0]
   #   pet = find_pet_by_name(@pet_shop,"Arthur")
-
+  #
   #   sell_pet_to_customer(@pet_shop, pet, customer)
-
+  #
   #   assert_equal(1, customer_pet_count(customer))
+  #   # we want the customer pet count to increase by 1
   #   assert_equal(1, pets_sold(@pet_shop))
+  #   # we want the pets sold in admin to increase by 1
   #   assert_equal(100, customer_cash(customer))
+  #   # customer cash is to be 100, ie decrease by 900
   #   assert_equal(1900, total_cash(@pet_shop))
+  #   # total cash is to increase by 1900, ie incrase by 900
   # end
 
   # def test_sell_pet_to_customer__pet_not_found
